3eb03a1aeef51dcf642a3782811eafe506cd40ab
Anthony Chung <75187566+anthonytchung@users.noreply.github.com>
2024-09-09 16:36:38 -0400

    ngrok front end

--- client/wearit-web/src/components/ImageUpload/index.tsx
+++ client/wearit-web/src/components/ImageUpload/index.tsx
@@ -30,7 +30,7 @@ export default function ImageUpload({ onUploadSuccess }: ImageUploadProps) {
       const formData = new FormData()
       formData.append('file', selectedImage)
 
-      const response = await axios.post('https://a7d0-173-79-43-34.ngrok-free.app/', formData, {
+      const response = await axios.post('https://a7d0-173-79-43-34.ngrok-free.app/api/upload', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       })
 
